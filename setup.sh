539b5c013948ac3b5e12cbc9bd3849d684fe3de0
high-u <zen.crazyd@gmail.com>
2017-10-30 16:50:52 +0000

    update setup.sh. completed

--- setup.sh
+++ setup.sh
@@ -13,7 +13,7 @@ curl -o- https://raw.githubusercontent.com/creationix/nvm/${NVM_VERSION}/install
 shopt -s expand_aliases
 source ~/.bashrc
 nvm ls-remote
-read -p "node version: " nv
+read -p "node version (ex. v8.8.1): " nv
 nvm install ${nv}
 nvm use ${nv}
 nvm alias default ${nv}
@@ -24,26 +24,30 @@ sudo yum install -y yum-utils device-mapper-persistent-data lvm2
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 sudo yum makecache fast
 yum list docker-ce.x86_64 --showduplicates
-read -p "docker version: " dv
+read -p "docker version (ex. 17.09.0.ce-1.el7.centos): " dv
 sudo yum install docker-ce-${dv}
 sudo systemctl enable docker
 sudo systemctl start docker
 
-# install node-red
-mkdir ~/node-red
+#  install node-red
+mkdir -p ~/node-red
 cd ~/node-red
-mkdir ./node_modules
+mkdir -p ~/node-red/user-settings
+mkdir -p ~/node-red/node_modules
 #npm install --save node-red@0.15.3
 npm install --save node-red
 
-nvm --version
-node -v
-npm -v
-docker --version
+echo "nvm "$(nvm --version)
+echo "node "$(node -v)
+echo "npm "$(npm -v)
+echo "docker "$(docker --version)
 node ~/node-red/node_modules/node-red/red.js --help
-echo "pm2 start ~/node_modules/node-red/red.js -u ~/node-red/user-dir -p 1880"
+echo ""
+echo -e 'ex. \e[44m pm2 start ~/node-red/node_modules/node-red/red.js -u ~/node-red/user-settings -p 1880 \e[m'
+echo ""
 
-# reboot
-read -p 'RUN `sudo reboot`: '
-sudo reboot
-
+# output message
+echo -e 'RUN \e[46m sudo reboot \e[m or \e[46m source ~/.bashrc \e[m'
+echo ""
+echo "success"
+exit 0
